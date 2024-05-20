4127c1b31faa212e2d34d572bc080a83468ab0d6
Alla QA <allushka181@gmail.com>
2024-05-20 09:57:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -0,0 +1,81 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ru.netology.delivery.data.DataGenerator;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan meeting")
+    void shouldSuccessfulPlanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 3;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 5;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[name='phone']").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(17));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
+        $(byText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?\n" +
+                        "\n" + "Перепланировать"))
+                .shouldBe(visible);
+        $("[data-test-id='replan-notification'] button[type='button'] .button__text").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+    @Test
+    @DisplayName("Should get error message if enter wrong phone number")
+    void shouldGetErrorIfWrongPhone() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 3;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generateWrongPhone("en"));
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $("[name='phone'].input_invalid .input__sub")
+                .shouldHave(exactText("Неверный формат номера мобильного телефона"));
+    }
+}
+
